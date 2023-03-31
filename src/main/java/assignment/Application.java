221dade14728c3876701a2fe7992bedee2586f23
Kanwal <akanwal@informatica.com>
2023-03-31 19:03:54 +0530

    Update

---
+++ src/main/java/assignment/Application.java
@@ -0,0 +1,25 @@
+package assignment;
+
+import assignment.model.InsurancePolicy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("assignment")
+@EntityScan("assignment")
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+//		Alien alien = context.getBean(Alien.class);
+//		alien.printHello();
+		InsurancePolicy insurancePolicy = context.getBean(InsurancePolicy.class);
+
+
+	}
+
+
+}
